55348839eec04e8799e20930a9f9ec8a97b56d77
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomAppointmentImageHitTest/CustomObjects.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+
+Namespace CustomAppointmentImageHitTest
+    #Region "#customappointment"
+    Public Class CustomAppointment
+        Public Property StartTime() As Date
+        Public Property EndTime() As Date
+        Public Property Subject() As String
+        Public Property Status() As Integer
+        Public Property Description() As String
+        Public Property Label() As Integer
+        Public Property Location() As String
+        Public Property AllDay() As Boolean
+        Public Property EventType() As Integer
+        Public Property RecurrenceInfo() As String
+        Public Property ReminderInfo() As String
+        Public Property OwnerId() As Object
+
+        Public Property Icon1() As Byte()
+        Public Property Icon2() As Byte()
+
+        Public Property AdditionalInfo() As String
+    End Class
+    #End Region '  #customappointment
+
+    #Region "#customresource"
+    Public Class CustomResource
+        Public Property Name() As String
+        Public Property ResID() As Integer
+        Public Property ResColor() As System.Drawing.Color
+        ' To display resources using the specified color set the SchedulerStorage.Resources.ColorSaving property to ColorSavingType.Color.
+    End Class
+    #End Region ' #customresource
+End Namespace
---
+++ VB/CustomAppointmentImageHitTest/MyMessageBox.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+
+Namespace CustomAppointmentImageHitTest
+    Partial Public Class MyMessageBox
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+        Public Sub New(ByVal img As Image, ByVal s As String)
+            InitializeComponent()
+            Me.pictureBox1.Image = img
+            Me.Text = s
+        End Sub
+    End Class
+End Namespace
